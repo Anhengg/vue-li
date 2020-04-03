e9f0cd43b8c2a85ff853a66062a26a83bdf75e42
Anhengg <1511191171@qq.com>
2020-04-03 22:18:03 +0800

    完成商品功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import {
   Button, Form, Input, FormItem, Message, Container, Header,
   Aside, Main, Footer, Menu, MenuItem, Submenu, MenuItemGroup, Breadcrumb, BreadcrumbItem,
-  Card, Row, Col, Table, TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox, Tag, Tree, Select, Option, Cascader, Alert, Tabs, TabPane
+  Card, Row, Col, Table, TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox, Tag, Tree, Select, Option, Cascader, Alert, Tabs, TabPane, Steps, Step, CheckboxGroup, Checkbox, Upload
 } from 'element-ui'
 
 Vue.use(Button)
@@ -37,6 +37,11 @@ Vue.use(Cascader)
 Vue.use(Alert)
 Vue.use(Tabs)
 Vue.use(TabPane)
+Vue.use(Steps)
+Vue.use(Step)
+Vue.use(CheckboxGroup)
+Vue.use(Checkbox)
+Vue.use(Upload)
 
 Vue.prototype.$confirm = MessageBox.confirm
 
