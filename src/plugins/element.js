509bbfe42cf1ab7a125a82abec4d123bedb4ef7d
Anhengg <1511191171@qq.com>
2020-04-04 20:07:09 +0800

    完成订单开发功能了

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import {
   Button, Form, Input, FormItem, Message, Container, Header,
   Aside, Main, Footer, Menu, MenuItem, Submenu, MenuItemGroup, Breadcrumb, BreadcrumbItem,
-  Card, Row, Col, Table, TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox, Tag, Tree, Select, Option, Cascader, Alert, Tabs, TabPane, Steps, Step, CheckboxGroup, Checkbox, Upload
+  Card, Row, Col, Table, TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox,
+  Tag, Tree, Select, Option, Cascader, Alert, Tabs, TabPane, Steps, Step, CheckboxGroup, Checkbox, Upload, Timeline, TimelineItem
 } from 'element-ui'
+
+// import Timeline from './timeline/index.js'
+// import TimelineItem from './timeline-item/index.js'
 
 Vue.use(Button)
 Vue.use(Form)
@@ -42,6 +46,8 @@ Vue.use(Step)
 Vue.use(CheckboxGroup)
 Vue.use(Checkbox)
 Vue.use(Upload)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
 
 Vue.prototype.$confirm = MessageBox.confirm
 
